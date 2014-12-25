4f5dd7c7768f394df5b966437c58dc541f572f0f
Anita <nitabunni@hotmail.com>
2014-12-25 18:05:42 -0500

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import characters
-import pinyin
+import sys
 
 print ("Hanyu Shuiping Kaoshi Level 6 (Advanced Chinese) \n")
 
-def display_choice():
-    display_choice=int(raw_input("Would you like to view words in characters or in pinyin? Press 1 for characters or 2 for pinyin. Press any other key to exit. \n"))
-    if display_choice==1:
-        characters.main()
-    elif display_choice==2:
-        pinyin.main()
-        
-    else:
-         print "Goodbye!"
-
-display_choice()
+display_choice=raw_input("Would you like to view words in characters or in pinyin? Press 1 for characters or 2 for pinyin. Press any other key to exit. \n")
+if display_choice=="1":
+    import characters
+    characters.main()
+elif display_choice=="2":
+    import pinyin
+    pinyin.main()
+else:
+    print "Goodbye!"
+    sys.exit()
